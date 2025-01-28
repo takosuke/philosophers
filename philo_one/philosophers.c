7cc758d6c0bdeac97fda2848304bf6282fbb745f
Mario C <gomibako@gomitorio.local>
2025-01-28 17:37:57 +0100

    scaffolding philo_01

---
+++ philo_one/philosophers.c
@@ -0,0 +1,64 @@
+#include "philosophers.h"
+
+t_restaurant	*init_restaurant(char **argv)
+{
+	t_restaurant	*restaurant;
+	restaurant = (t_restaurant *)malloc(sizeof(t_restaurant));
+	if (restaurant == NULL)
+		return (NULL);
+	restaurant->num_philosophers = atoi(argv[1]);
+	restaurant->time_to_die = atoi(argv[2]);
+	restaurant->time_to_eat = atoi(argv[3]);
+	restaurant->time_to_sleep = atoi(argv[4]);
+	return (restaurant);
+}
+
+void	init_forks(t_restaurant *restaurant)
+{
+	long	i;
+	restaurant->forks = (t_fork *)malloc(restaurant->num_philosophers * sizeof(t_fork));
+
+	i = 0;
+	while (i < restaurant->num_philosophers)
+	{
+		restaurant->forks[i].id = i;
+		printf("%ld\n", i);
+		i++;
+	}
+}
+
+void	init_philosophers(t_restaurant *restaurant)
+{
+	long	i;
+
+	restaurant->philosophers = (t_philo *)malloc(restaurant->num_philosophers * sizeof(t_philo));
+
+	i = 0;
+	while (i < restaurant->num_philosophers)
+	{
+		restaurant->philosophers[i].id = i;
+		printf("%ld\n", i);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	t_restaurant	*restaurant;
+//	if (argc == 6)
+//	set a max_meals option or something
+	if (argc > 4)
+	{
+		restaurant = init_restaurant(argv);
+		printf("Restaurant num_philosophers: %ld\n", restaurant->num_philosophers);
+		init_forks(restaurant);
+		init_philosophers(restaurant);
+		int i = 0;
+		while (i < restaurant->num_philosophers)
+		{
+			printf("Philosopher id: %d\n", restaurant->philosophers[i].id);
+			i++;
+		}
+	}
+	return (0);
+}
--- philo_one/philosophers.h
+++ philo_one/philosophers.h
@@ -1,8 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/time.h>
 
+//typedef struct philosopher_s philosopher_t;
+
+typedef struct	s_philo
+{
+	int	id;
+} t_philo;
+
+typedef struct	s_fork
+{
+	int	id;
+}	t_fork;
+
+typedef struct	s_restaurant
+{
+	t_philo		*philosophers;
+	t_fork		*forks;
+	long		num_philosophers;
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		max_meals;
+}	t_restaurant;
+
+#endif
 
